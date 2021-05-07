470c4d87615842c52d05c44bf9cc80fb287bfdc9
sunkanmicode <oguntimehins@gmail.com>
2021-05-07 04:31:32 -0700

    adding client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react'
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Redirect 
+} from 'react-router-dom';
+import { toast } from 'react-toastify';
+// import 'react-toastify/dist/react-toastify.css';
+
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+
 import './App.css';
 
+toast.configure();
+
 function App() {
+
+  const [isAuth, setIsAuth ] = useState(false);
+
+  const setAuth = (boolean)=>{
+      setIsAuth(boolean)
+  };
+
+  const isVerified = async()=>{
+    try {
+      const response = await fetch('http://localhost:4000/auth/is-verify',{
+        method: 'GET',
+        headers: {token: localStorage.token}
+      });
+
+      const data = await response.json();
+      console.log(data);
+      data === true? setIsAuth(true): setIsAuth(false)
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(()=>{
+    isVerified();
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <div className='container'>
+        <Switch>
+          <Route exact path ='/login' 
+            render={props =>
+                  !isAuth?( <Login {...props} 
+                    setAuth={setAuth} />
+                ):(
+                  <Redirect to = '/dashboard' />
+                )
+              } 
+          />
+
+          <Route exact path ='/register' 
+            render={props => 
+                  !isAuth? (
+                  <Register {...props}
+                  setAuth={setAuth} />
+                ):(
+                  <Redirect to='/login' />
+                )
+              } 
+          />
+
+          <Route exact path ='/dashboard' 
+            render={props => 
+                  isAuth? (
+                  <Dashboard {...props} 
+                  setAuth={setAuth}/>
+                ):(
+                  <Redirect to="/login" />
+                )
+              } 
+          />
+
+        </Switch>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
